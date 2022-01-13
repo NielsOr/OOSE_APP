8f7a91742a40a6342f5aab805f77bdd749e3d047
tvloet1 <t.vloet1@student.han.nl>
2022-01-13 21:17:09 +0100

    Created first unit tests for EvlService.
    First unit tests. Don't get mad.

---
+++ LOGIC.Tests/ServicesTests/EvlServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using LOGIC.Interfaces.Repositories;
+using LOGIC.Models;
+using LOGIC.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LOGIC.Tests.ServicesTests
+{
+    public class EvlServiceTests
+    {
+
+        [Fact]
+        public async Task CreateEvlTests()
+        {
+            // Arrange
+            var evl = new Evl();
+            var EvlRepository = new Mock<IEvlRepository>();
+            EvlRepository.Setup(x => x.Create(evl)).ReturnsAsync(evl);
+            var sut = new EvlService(EvlRepository.Object);
+
+            // Act
+            var result = await sut.CreateEvl(evl);
+
+            // Assert
+            Assert.True(result.Success);
+        }
+    }
+
+}
