3bb746be7237c8a6c007695cc2600134d0e7ba7a
tvloet1 <t.vloet1@student.han.nl>
2022-01-18 18:56:29 +0100

    Create RubricServiceTests.cs
    UT for RubricService.CreateRubric

---
+++ LOGIC.Tests/ServicesTests/RubricServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using LOGIC.Interfaces.Repositories;
+using LOGIC.Models;
+using LOGIC.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LOGIC.Tests.ServicesTests
+{
+    public class RubricServiceTests
+    {
+        [Fact]
+        public async Task CreateRubricTests()
+        {
+            // Arrange
+            var rubric = new Rubric();
+            var RubricRepository = new Mock<IRubricRepository>();
+            RubricRepository.Setup(x => x.Create(rubric)).ReturnsAsync(rubric);
+            var sut = new RubricService(RubricRepository.Object);
+
+            // Act
+            var result = await sut.CreateRubric(rubric);
+
+            // Assert
+            Assert.True(result.Success);
+        }
+    }
+}
