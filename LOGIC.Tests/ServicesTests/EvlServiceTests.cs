bf3a6c1fbdf5a72522d701ecc36b53b31f33a4c0
Roel Stevens <38633494+RMWMStevens@users.noreply.github.com>
2022-01-18 20:55:53 +0100

    Created Blazor WebAssembly project

--- LOGIC.Tests/ServicesTests/EvlServiceTests.cs
+++ LOGIC.Tests/ServicesTests/EvlServiceTests.cs
@@ -2,10 +2,6 @@
 using LOGIC.Models;
 using LOGIC.Services;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
--- LOGIC.Tests/ServicesTests/TentamineringServiceTests.cs
+++ LOGIC.Tests/ServicesTests/TentamineringServiceTests.cs
@@ -3,10 +3,6 @@ using LOGIC.Interfaces.Repositories;
 using LOGIC.Models;
 using LOGIC.Services;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
